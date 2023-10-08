318cc25d60ebf5c0865124a242a3b38d1fd12967
MotiCAT <youyuzhangye@gmail.com>
2023-10-08 22:28:32 +0900

    chore: remove debug command

--- src/index.js
+++ src/index.js
@@ -35,20 +35,4 @@ client.on('guildMemberRemove', async (member) => {
     channel.send({ embeds: [embed] });
 });
 
-client.on('messageCreate', async (message) => {
-    if (message.author.bot || message.author.id !== '895050958441160734') return;
-    if (message.content === 'chp!test') {
-        const member = message.member;
-        const embed = new Discord.EmbedBuilder()
-            .setTitle('Welcome to CatHouse Products!')
-            .setDescription(`CatHouse Productsへようこそ, ${member.user}!\nまずは <#1058665600517410886> を読んでください！\n\nWelcome to CatHouse Products, ${member.user}!\nPlease read <#1058665600517410886> first!`)
-            .setColor('#3CB371')
-            .setThumbnail(member.user.displayAvatarURL())
-            .setTimestamp()
-            .setFooter({ text: 'CatHouse Products', iconURL: client.user.displayAvatarURL() });
-        const channel = await member.guild.channels.fetch('1160545808811315200');
-        channel.send({ embeds: [embed] });
-    }
-})
-
 client.login(process.env.TOKEN);
